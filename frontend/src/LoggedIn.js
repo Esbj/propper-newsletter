62922f04c6504611b5df774e5372322b96292013
Alexander <alex.esbj@gmail.com>
2020-06-02 16:02:08 +0200

    React sklett på front end

---
+++ frontend/src/LoggedIn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+class LoggedIn extends React.Component {
+    render(){
+
+        return(
+            <div> Nu är du inloggad!</div>
+        )
+    }
+}
+
+export default LoggedIn;
---
+++ frontend/src/StartPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+class StartPage extends React.Component{
+    render(){
+
+        return(
+            <div>du behöver logga in!</div>
+        )
+    }
+}
+
+export default StartPage;
